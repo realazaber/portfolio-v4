5b991ae46f67da3d231aac107991d49ffa3c5f4d
therealcoolpup <47402666+therealcoolpup@users.noreply.github.com>
2023-04-18 14:21:39 +0200

    About page draft

---
+++ src/components/Sections/About.tsx
@@ -0,0 +1,42 @@
+import Image from "next/image";
+import { Typewriter } from "react-simple-typewriter";
+import Slide from "react-reveal/Slide";
+
+export default function About() {
+  return (
+    <section id="about" className="border-2 border-red-500">
+      <Image
+        src="/images/hello-there.webp"
+        width={100}
+        height={100}
+        alt="Hello There"
+        className="w-full h-auto object-contain"
+      ></Image>
+      <div className="flex flex-row gap-3 justify-center items-center">
+        <div className="w-2/5 flex justify-end">
+          <Image src="/images/me.webp" width={300} height={300} />
+        </div>
+        <div className="w-3/5">
+          <div className="text-2xl text-center">
+            I am a{" "}
+            <Typewriter
+              words={["problem solver", "software engineer", "Nerd"]}
+              loop={0}
+              cursor
+              cursorStyle="_"
+              typeSpeed={70}
+              deleteSpeed={100}
+              delaySpeed={1000}
+            />
+          </div>
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aspernatur
+            ut unde vero odit. Dolorum maiores at, repudiandae eum deleniti
+            accusamus! Consectetur animi id sequi fugiat eum, at enim quos
+            reiciendis?
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+}
