2463bf3899776bff6cad65cd2f2e7b225abc8e51
therealcoolpup <47402666+therealcoolpup@users.noreply.github.com>
2023-04-20 20:21:49 +0200

    Base nav

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,4 +1,6 @@
 import { useState } from "react";
+import Image from "next/image";
+
 
 export default function Nav() {
   const [open, setOpen] = useState(false);
@@ -10,13 +12,21 @@ export default function Nav() {
   };
 
   return (
-    <nav className="p-3 w-full flex flex-row justify-between">
-      <a onClick={() => scrollToSection("home")}>Home</a>
-      <div className="flex flex-row gap-3">
+    <nav className="p-3 w-full flex flex-row justify-between items-center fixed top-0 left-0 right-0 bg-white z-10">
+      <a onClick={() => scrollToSection("home")}>
+        <Image className="object-cover w-64 h-24" src="/images/azaber.webp" width={100} height={100} />
+      </a>
+      <div className="flex flex-row gap-5 items-center text-pri h-24">
         <a onClick={() => scrollToSection("about")}>About</a>
+        <a onClick={() => scrollToSection("experience")}>Experience</a>
         <a onClick={() => scrollToSection("projects")}>Projects</a>
-        <a href="/blog">Blog</a>
-        <a onClick={() => scrollToSection("contact")}>Contact</a>
+        <a href="/blog">Blog <i className="fas fa-external-link-alt"></i></a>
+        <a
+          className="btn"
+          onClick={() => scrollToSection("contact")}
+        >
+          Contact
+        </a>
       </div>
     </nav>
   );
