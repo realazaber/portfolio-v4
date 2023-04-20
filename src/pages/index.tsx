be3d12d4bb1a8bc3d3dcc02b3b7d6a8a38c2f6a9
therealcoolpup <47402666+therealcoolpup@users.noreply.github.com>
2023-04-20 20:22:20 +0200

    Added all sections to main

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,19 @@
 import Home from "@/components/Sections/Home";
 import About from "@/components/Sections/About";
+import Experience from "@/components/Sections/Experience";
 import Projects from "@/components/Sections/Projects";
+import Testimonials from "@/components/Sections/Testimonials";
 import Contact from "@/components/Sections/Contact";
+
 
 export default function Index() {
   return (
     <>
       <Home />
       <About />
+      <Experience />
       <Projects />
+      <Testimonials />
       <Contact />
     </>
   );
