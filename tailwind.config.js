dfef0ca29f84bc0ffa609bc8b802b0e7fabbe9ef
therealcoolpup <47402666+therealcoolpup@users.noreply.github.com>
2023-04-21 00:23:46 +0200

    Breakpoints

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,19 @@ module.exports = {
         sec: "#ffffff",
         accent: "#ababae",
       },
+      screens: {
+        phone: "600px",
+        // => @media (min-width: 600px) { ... }
+
+        tablet: "850px",
+        // => @media (min-width: 850px) { ... }
+
+        laptop: "1024px",
+        // => @media (min-width: 1024px) { ... }
+
+        desktop: "1280px",
+        // => @media (min-width: 1280px) { ... }
+      },
     },
   },
   plugins: [],
