8c19a650bc4c901770c253a1e20e4c96e85c0b62
therealcoolpup <47402666+therealcoolpup@users.noreply.github.com>
2023-04-21 13:52:19 +0200

    Updated breakpoints

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,11 +16,8 @@ module.exports = {
         phone: "600px",
         // => @media (min-width: 600px) { ... }
 
-        tablet: "850px",
+        tablet: "1000px",
         // => @media (min-width: 850px) { ... }
-
-        laptop: "1024px",
-        // => @media (min-width: 1024px) { ... }
 
         desktop: "1280px",
         // => @media (min-width: 1280px) { ... }
