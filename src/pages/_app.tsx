aa0f59dc727201a6caf2a3f64394fbc93fe686e0
therealcoolpup <47402666+therealcoolpup@users.noreply.github.com>
2023-04-21 13:52:39 +0200

    Top margin to avoid going under the nav

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,7 +7,7 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Nav />
-      <div className="flex flex-col justify-center items-center w-full mx-auto">
+      <div className="flex flex-col justify-center items-center w-full mx-auto mt-32 overflow-x-hidden">
         <Component {...pageProps} />
       </div>
       <Footer />
