2297dd8056f8e158c74cedad7795f6297c8ee4fe
therealcoolpup <47402666+therealcoolpup@users.noreply.github.com>
2023-04-22 16:04:00 +0200

    Base contact form

--- src/components/Sections/Contact.tsx
+++ src/components/Sections/Contact.tsx
@@ -1,3 +1,43 @@
 export default function Contact() {
-  return <section id="contact">contact</section>;
+  return (
+    <section id="contact">
+      <div className="container">
+        <h2>Contact</h2>
+        <form
+          className="flex flex-col justify-center items-center w-3/5 mx-auto"
+          action="https://formspree.io/f/mayzabaq"
+          method="POST"
+        >
+          <label>
+            <span>
+              Name <b>*</b>
+            </span>
+            <input type="text" name="name" required />
+          </label>
+          <label>
+            <span>
+              Email <b>*</b>
+            </span>
+            <input type="email" name="email" required />
+          </label>
+
+          <label>
+            <span>Phone</span>
+            <input type="number" name="phone" />
+          </label>
+
+          <label>
+            <span>
+              Message <b>*</b>
+            </span>
+            <textarea name="message" required></textarea>
+          </label>
+
+          <button className="btn" type="submit">
+            Submit
+          </button>
+        </form>
+      </div>
+    </section>
+  );
 }
