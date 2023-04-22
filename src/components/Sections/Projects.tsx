27aa83aa36e1528e21e3b1f9381e7cff40fb43bd
therealcoolpup <47402666+therealcoolpup@users.noreply.github.com>
2023-04-22 16:04:11 +0200

    Created base projects section

--- src/components/Sections/Projects.tsx
+++ src/components/Sections/Projects.tsx
@@ -1,3 +1,9 @@
 export default function Projects() {
-  return <section id="projects">projects</section>;
+  return (    
+    <section id="projects">
+      <div className="container text-pri">
+        <h2>Projects</h2>
+      </div>
+    </section>
+  );
 }
