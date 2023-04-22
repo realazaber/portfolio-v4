56d05d333f85bc73d6dab8dd106a5207d5f6cc36
therealcoolpup <47402666+therealcoolpup@users.noreply.github.com>
2023-04-22 16:14:15 +0200

    Contact page done
    Connected to form spree and has thank you message

--- src/components/Sections/Contact.tsx
+++ src/components/Sections/Contact.tsx
@@ -1,12 +1,36 @@
+import { useForm, ValidationError } from "@formspree/react";
+import Image from "next/image";
+import Slide from "react-reveal/Slide";
+
 export default function Contact() {
+  const [state, handleSubmit] = useForm("mayzabaq");
+
+  if (state.succeeded) {
+    return (
+      <section id="contact">
+        <div className="container text-center flex flex-col justify-center items-center gap-5">
+          <Slide up>
+            <h2>Thanks for contacting me!</h2>
+            <Image
+              className="w-52 h-52 object-contain"
+              src={"/images/me.webp"}
+              height={300}
+              width={300}
+              alt="me"
+            />
+          </Slide>
+        </div>
+      </section>
+    );
+  }
+
   return (
     <section id="contact">
       <div className="container">
         <h2>Contact</h2>
         <form
           className="flex flex-col justify-center items-center w-3/5 mx-auto"
-          action="https://formspree.io/f/mayzabaq"
-          method="POST"
+          onSubmit={handleSubmit}
         >
           <label>
             <span>
@@ -14,11 +38,16 @@ export default function Contact() {
             </span>
             <input type="text" name="name" required />
           </label>
-          <label>
+          <label htmlFor="email">
             <span>
               Email <b>*</b>
             </span>
-            <input type="email" name="email" required />
+            <input id="email" type="email" name="email" required />
+            <ValidationError
+              prefix="Email"
+              field="email"
+              errors={state.errors}
+            />
           </label>
 
           <label>
@@ -26,14 +55,19 @@ export default function Contact() {
             <input type="number" name="phone" />
           </label>
 
-          <label>
+          <label htmlFor="message">
             <span>
               Message <b>*</b>
             </span>
-            <textarea name="message" required></textarea>
+            <textarea id="message" name="message" required />
+            <ValidationError
+              prefix="Message"
+              field="message"
+              errors={state.errors}
+            />
           </label>
 
-          <button className="btn" type="submit">
+          <button className="btn" type="submit" disabled={state.submitting}>
             Submit
           </button>
         </form>
