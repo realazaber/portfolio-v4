59d723e5a0ca908ab6af88f163d040cc2850d782
therealcoolpup <47402666+therealcoolpup@users.noreply.github.com>
2023-04-23 17:33:24 +0200

    Footer skip to section links

--- src/components/Navigation/Footer.tsx
+++ src/components/Navigation/Footer.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import { useState } from "react";
 
 export default function Footer() {
@@ -12,13 +13,41 @@ export default function Footer() {
   return (
     <footer className="flex flex-row justify-between p-3">
       <div>
-        <a onClick={() => scrollToSection("home")}>Home</a>
+        <a
+          className="text-pri hover:text-blue-700"
+          onClick={() => scrollToSection("home")}
+        >
+          <Image
+            className="object-cover w-64 h-24"
+            src="/images/azaber.webp"
+            width={100}
+            height={100}
+            alt="logo"
+          />
+        </a>
       </div>
       <div className="flex flex-col gap-3 text-right">
-        <a onClick={() => scrollToSection("about")}>About</a>
-        <a onClick={() => scrollToSection("projects")}>Projects</a>
-        <a href="/blog">Blog</a>
-        <a onClick={() => scrollToSection("contact")}>Contact</a>
+        <a
+          className="text-pri hover:text-blue-700"
+          onClick={() => scrollToSection("about")}
+        >
+          About
+        </a>
+        <a
+          className="text-pri hover:text-blue-700"
+          onClick={() => scrollToSection("projects")}
+        >
+          Projects
+        </a>
+        <a className="text-pri hover:text-blue-700" href="/blog">
+          Blog
+        </a>
+        <a
+          className="text-pri hover:text-blue-700"
+          onClick={() => scrollToSection("contact")}
+        >
+          Contact
+        </a>
       </div>
     </footer>
   );
