5c183a719213af6bdf9f6705ced42fbf4705db0b
swagoverlord <azcooldude8@gmail.com>
2023-04-28 09:45:05 +0200

    Test project

--- src/components/Sections/Projects.tsx
+++ src/components/Sections/Projects.tsx
@@ -1,8 +1,11 @@
+import Project from "../Misc/Project";
+
 export default function Projects() {
-  return (    
+  return (
     <section id="projects">
       <div className="container text-pri">
         <h2>Projects</h2>
+        <Project github="github.com" deployment="test.com" />
       </div>
     </section>
   );
