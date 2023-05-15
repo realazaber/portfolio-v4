51290791023c99dc57c0295df852a2fc7b817d17
swagoverlord <azcooldude8@gmail.com>
2023-05-15 14:41:27 +0200

    Bug fix for project id page

--- src/pages/projects/[id].tsx
+++ src/pages/projects/[id].tsx
@@ -11,18 +11,45 @@ export default function Project() {
 
   useEffect(() => {
     async function fetchProject() {
-      const res = await fetch(`${baseUrl}/project/${id}`);
-      const data = await res.json();
-      setProject({
-        id: id,
-        title: data.acf.title,
-        description: data.acf.description,
-        mainImg: data.acf.image,
-        github: data.acf.github,
-        deployment: data.acf.deploy_link,
-        tech: data.acf.tech.map((t: any) => t.name),
-        isMainProject: data.acf.is_main_project === "true",
-      });
+      if (Array.isArray(id)) {
+        // Handle the case where id is an array
+        // For simplicity, consider using only the first element in the array
+        const firstId = id[0];
+        const res = await fetch(`${baseUrl}/project/${firstId}`);
+        const data = await res.json();
+        if (data) {
+          setProject({
+            id: parseInt(firstId),
+            acf: {
+              title: data.acf.title,
+              image: data.acf.image,
+              github: data.acf.github,
+              deploy_link: data.acf.deploy_link,
+              tech: data.acf.tech.map((t: any) => t.name),
+              description: data.acf.description,
+              is_main_project: data.acf.is_main_project,
+            },
+          });
+        }
+      } else if (typeof id === "string") {
+        // Handle the case where id is a string
+        const res = await fetch(`${baseUrl}/project/${id}`);
+        const data = await res.json();
+        if (data) {
+          setProject({
+            id: parseInt(id),
+            acf: {
+              title: data.acf.title,
+              image: data.acf.image,
+              github: data.acf.github,
+              deploy_link: data.acf.deploy_link,
+              tech: data.acf.tech.map((t: any) => t.name),
+              description: data.acf.description,
+              is_main_project: data.acf.is_main_project,
+            },
+          });
+        }
+      }
     }
 
     if (id) {
@@ -39,37 +66,40 @@ export default function Project() {
       <div className="relative w-full h-[50vh]">
         <img
           className="w-full h-full object-cover z-10"
-          src={project.mainImg}
-          alt={project.title}
+          src={project.acf.image}
+          alt={project.acf.title}
         />
         <div className="absolute px-3 top-0 left-0 right-0 bottom-0 w-3/5 mx-auto h-[inherit] z-30 flex flex-wrap justify-center items-center text-white">
           <div className="w-1/2">
-            <h1 className="text-white font-semibold">{project.title}</h1>
+            <h1 className="text-white font-semibold">{project.acf.title}</h1>
           </div>
           <div className="w-1/2 text-right">
             {" "}
             <p>Technologies:</p>
             <ul className="flex flex-col gap-3">
-              {project.tech.map((tech, index) => (
-                <li key={index}>{tech}</li>
+              {project.acf.tech.map((techName, index) => (
+                <>
+                  <>{techName}</>
+                  <br />
+                </>
               ))}
             </ul>
           </div>
           <div className="w-full flex flex-row justify-center items-center gap-3">
-            {project.github !== "" && (
+            {project.acf.github !== "" && (
               <Link
                 className="text-white text-lg flex gap-1 items-center justify-center"
-                href={project.github}
+                href={project.acf.github}
                 target="_blank"
               >
                 Github link:
                 <i className="fa-brands fa-github text-white"></i>
               </Link>
             )}
-            {project.deployment !== "" && (
+            {project.acf.deploy_link !== "" && (
               <Link
                 className="text-white text-lg flex gap-1 items-center justify-center"
-                href={project.deployment}
+                href={project.acf.deploy_link}
                 target="_blank"
               >
                 Deployment link:
@@ -84,7 +114,7 @@ export default function Project() {
       <div className="container my-3">
         <div
           className="text-accent"
-          dangerouslySetInnerHTML={{ __html: project.description }}
+          dangerouslySetInnerHTML={{ __html: project.acf.description }}
         />
       </div>
     </>
