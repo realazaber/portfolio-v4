b9ab34cc16ee1015ff9086892ee6f293352bdfdd
swagoverlord <azcooldude8@gmail.com>
2023-05-15 12:10:10 +0200

    Modules for external untyped libraries

---
+++ src/types/VerticalTimeline.d.ts
@@ -0,0 +1,5 @@
+declare module "react-vertical-timeline-component" {
+  const VerticalTimeline: any;
+  const VerticalTimelineElement: any;
+  export { VerticalTimeline, VerticalTimelineElement };
+}
