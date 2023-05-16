4a03ccaa962eae303e5490dcb2673429a6aa54c8
swagoverlord <azcooldude8@gmail.com>
2023-05-16 18:07:26 +0200

    error page

--- next.config.js
+++ next.config.js
@@ -1,14 +1,7 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  output: "export",
-};
-
-module.exports = nextConfig;
-
 const path = require("path");
 
 module.exports = {
+  reactStrictMode: true,
   webpack: (config) => {
     config.resolve.alias["@"] = path.resolve(__dirname, "./src");
     return config;
