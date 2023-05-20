be2c9a18d5a7ea5978bfe75d6009c0eb766a4344
swagoverlord <azcooldude8@gmail.com>
2023-05-20 10:53:17 +0200

    Mobile nav works

--- src/components/Navigation/MobileNav.tsx
+++ src/components/Navigation/MobileNav.tsx
@@ -1,7 +1,55 @@
+import React, { useState } from "react";
+import Link from "next/link";
+import { useRouter } from "next/router";
+
 export default function MobileNav() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [open, setOpen] = useState(false);
+  const router = useRouter();
+
+  const scrollToSection = (id: any) => {
+    setIsMenuOpen(!isMenuOpen);
+    if (router.pathname !== "/") {
+      router.push("/");
+    }
+    const element = document.getElementById(id);
+    element?.scrollIntoView({ behavior: "smooth" });
+    setOpen(false);
+  };
+
   return (
-    <div className="tablet:hidden tablet:invisible fixed top-5 right-5">
-      <i className="fas fa-hamburger text-pri hover:text-blue-600 text-5xl"></i>
-    </div>
+    <>
+      <div
+        className={`fixed top-0 right-0 bottom-0 z-50 bg-white w-full transform duration-300 ease-in-out ${
+          isMenuOpen ? "translate-x-0" : "translate-x-full"
+        }`}
+      >
+        <div
+          id="mobileNav"
+          className="flex flex-col h-full w-full opacity-80 justify-center items-center p-4"
+        >
+          <a onClick={() => scrollToSection("home")}>Home</a>
+
+          <a onClick={() => scrollToSection("about")}>About</a>
+          <a onClick={() => scrollToSection("experience")}>Experience</a>
+          <a onClick={() => scrollToSection("projects")}>Projects</a>
+          <Link
+            href="https://personal-blog-ten-bice.vercel.app/"
+            target="_blank"
+          >
+            Blog <i className="fas fa-external-link-alt"></i>
+          </Link>
+
+          <a onClick={() => scrollToSection("contact")}>Contact</a>
+        </div>
+      </div>
+
+      <nav className="tablet:hidden tablet:invisible fixed top-5 right-5">
+        <i
+          className="fas fa-hamburger text-pri hover:text-blue-600 text-5xl"
+          onClick={() => setIsMenuOpen(!isMenuOpen)}
+        ></i>
+      </nav>
+    </>
   );
 }
