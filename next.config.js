29db82aacca3aa57620b320cadd871ecacb63d64
therealcoolpup <azcooldude8@gmail.com>
2023-06-23 21:40:17 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@ const path = require("path");
 
 module.exports = {
   reactStrictMode: true,
+  trailingSlash: true,
   webpack: (config) => {
     config.resolve.alias["@"] = path.resolve(__dirname, "./src");
     return config;
